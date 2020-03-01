4445aacb33ab63103d5bef41cd9b56a984b598a9
Mathew Jones <anilox123@gmail.com>
2020-03-01 14:11:23 -0600

    added Add method

---
+++ BinaryTree/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Tree tree = new Tree();
+            
+
+            tree.Add(5);
+
+        }
+    }
+}
---
+++ BinaryTree/Tree.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTree
+{
+    public class Tree
+    {
+        public Node root;
+
+        public Tree()
+        {
+
+
+            Node root = null;
+
+        }
+        
+        public Node Add( int v)
+        {
+            
+            if(root == null)
+            {
+                root = new Node();
+                root.value = v;
+
+                return root;                
+            }
+            else if (v < root.value)
+            {
+                root.left = new Node();
+                root.left.value = v;
+                return root.left;
+            }
+            else
+            {
+                root.right = new Node();
+                root.right.value = v;
+                return root.right;
+            }
+
+
+
+            return root;
+
+
+
+            //else if(root < root.value)
+            //{ew
+            //    Node left = new Node(node.value);
+
+            //}
+            //else 
+            //{
+            //    Node right = new Node(node.value);
+
+            //}
+
+
+        }
+
+    }
+}
