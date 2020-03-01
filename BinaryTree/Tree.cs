553b9c0cd26c38f2c73de5bc8189520fe07d1d57
Mathew Jones <anilox123@gmail.com>
2020-03-01 14:23:25 -0600

    have an error I don't understand

--- BinaryTree/Tree.cs
+++ BinaryTree/Tree.cs
@@ -12,51 +12,37 @@ namespace BinaryTree
 
         public Tree()
         {
-
-
-            Node root = null;
+           
 
         }
         
         public Node Add( int v)
         {
-            
+
+            Node root = new Node();
+
             if(root == null)
             {
-                root = new Node();
                 root.value = v;
+
 
                 return root;                
             }
             else if (v < root.value)
             {
-                root.left = new Node();
                 root.left.value = v;
+
                 return root.left;
             }
             else
             {
-                root.right = new Node();
                 root.right.value = v;
+
                 return root.right;
             }
 
 
 
-            return root;
-
-
-
-            //else if(root < root.value)
-            //{ew
-            //    Node left = new Node(node.value);
-
-            //}
-            //else 
-            //{
-            //    Node right = new Node(node.value);
-
-            //}
 
 
         }
